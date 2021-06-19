6498d2db40b471cbaa454e0ffab732fe9fbeb70a
karthi-karthik <84576438+karthi-karthik@users.noreply.github.com>
2021-06-19 18:07:43 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,11 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int a[],int l){
+  int max_num=0;
+  for(i=0;i<l;i++){
+    if(a[i]>max_num){
+      max_num=a[i];
+    }
+    return max_num;
+  }
+  
